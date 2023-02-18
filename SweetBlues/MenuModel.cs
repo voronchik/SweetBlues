324c6a78dae2e38a6b7db51a65c491119340cab7
Alex V <alexnikvorona@gmail.com>
2023-02-18 15:46:07 +0200

    Додав розділи в Меню, додав стрічку новин

--- SweetBlues/MenuModel.cs
+++ SweetBlues/MenuModel.cs
@@ -9,23 +9,108 @@ namespace SweetBlues
 {
 	public static class MenuModel
 	{
+		//структура меню Головне меню
 		public static IReplyMarkup GetMainMenu()
 		{
 			KeyboardButton[][] buttons = new KeyboardButton[][]
 			{
-				new KeyboardButton[]{new KeyboardButton("Далі -->")},
+				new KeyboardButton[]{new KeyboardButton("Меню"), new KeyboardButton("Стрічка новин")},
 				new KeyboardButton[]{new KeyboardButton("Зарахувати бонуси"), new KeyboardButton("Мій профіль")},
+				new KeyboardButton[]{new KeyboardButton("Задонатити на ЗСУ!!!")}
 			};
 			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
 			return keyboard;
 		}
 
+		//структура меню Меню
+		public static IReplyMarkup GetMenuMenu()
+		{
+			KeyboardButton[][] buttons = new KeyboardButton[][]
+			{
+				new KeyboardButton[]{new KeyboardButton("Напої"), new KeyboardButton("Тортики")},
+				new KeyboardButton[]{new KeyboardButton("Фірмове печіво"), new KeyboardButton("Морозиво")},
+				new KeyboardButton[]{new KeyboardButton("<-- Назад до головного меню") }
+			};
+			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
+			return keyboard;
+		}
+
+		//структура меню Стрічка новин
+		public static IReplyMarkup GetNewsFeed()
+		{
+			KeyboardButton[][] buttons = new KeyboardButton[][]
+			{
+				new KeyboardButton[]{new KeyboardButton("<-- Назад до головного меню") }
+			};
+			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
+			return keyboard;
+		}
+
+		//структура меню Зарахувати бонуси
+		public static IReplyMarkup GetCreditBonusesMenu()
+		{
+			KeyboardButton[][] buttons = new KeyboardButton[][]
+			{
+				
+			};
+			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
+			return keyboard;
+		}
+
+		//структура меню Мій профіль
 		public static IReplyMarkup GetMyProfileMenu()
 		{
 			KeyboardButton[][] buttons = new KeyboardButton[][]
 			{
 				new KeyboardButton[]{new KeyboardButton("Оновити дані"), new KeyboardButton("Мої бонуси")},
-				new KeyboardButton[]{new KeyboardButton("<-- Повернутися до головного меню")},
+				new KeyboardButton[]{new KeyboardButton("<-- Назад до головного меню"), new KeyboardButton("Зробіть нас кращіми!")}
+			};
+			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
+			return keyboard;
+		}
+
+		//структура меню Задонатити на ЗСУ
+		public static IReplyMarkup GetDonateArmyMenu()
+		{
+			KeyboardButton[][] buttons = new KeyboardButton[][]
+			{
+				new KeyboardButton[]{new KeyboardButton("<-- Назад до головного меню")}
+			};
+			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
+			return keyboard;
+		}
+
+		//структура меню Мій профіль -> Оновити дані
+		public static IReplyMarkup GetUpdateDataMenu()
+		{
+			KeyboardButton[][] buttons = new KeyboardButton[][]
+			{
+				new KeyboardButton[]{new KeyboardButton("Ім'я та прізвище")},
+				new KeyboardButton[]{new KeyboardButton("Номер телефону"), new KeyboardButton("Email")},
+				new KeyboardButton[]{new KeyboardButton("<-- Назад до мого профілю")}
+			};
+			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
+			return keyboard;
+		}
+
+		//структура меню Мій профіль -> Мої бонуси
+		public static IReplyMarkup GetMyBonusMenu()
+		{
+			KeyboardButton[][] buttons = new KeyboardButton[][]
+			{
+				new KeyboardButton[]{new KeyboardButton("Історія нарахування"), new KeyboardButton("Використати бонуси")},
+				new KeyboardButton[]{new KeyboardButton("<-- Назад до мого профілю")}
+			};
+			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
+			return keyboard;
+		}
+
+		//структура меню Мій профіль -> Зробіть нас кращіми
+		public static IReplyMarkup GetMakeUsBetterMenu()
+		{
+			KeyboardButton[][] buttons = new KeyboardButton[][]
+			{
+				new KeyboardButton[]{new KeyboardButton("<-- Назад до мого профілю")}
 			};
 			ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(buttons);
 			return keyboard;
--- SweetBlues/Program.cs
+++ SweetBlues/Program.cs
@@ -18,7 +18,7 @@ namespace SweetBlues
 
 		static void Main(string[] args)
 		{
-			CancellationTokenSource source= new CancellationTokenSource();
+			CancellationTokenSource source = new CancellationTokenSource();
 			CancellationToken cancel = source.Token;
 			ReceiverOptions options = new ReceiverOptions()
 			{
@@ -33,30 +33,81 @@ namespace SweetBlues
 		static async Task GetMessage(Message message)
 		{
 			string mess = message.Text.ToLower();
+			
+			//вхід в головне меню
 			if (mess == "/start")
 			{
 				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Радий Вас вітати у нашій кав'ярні!",
 					replyMarkup: MenuModel.GetMainMenu());
-
 			}
+
+			//Перехід до меню Меню
+			else if (mess == "меню")
+			{
+				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Сподіваємось Ви знайдете для себе щось смачненьке.",
+					replyMarkup: MenuModel.GetMenuMenu());
+			}
+
+			//Перехід до меню Лента новин
+			else if (mess == "стрічка новин")
+			{
+				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "А тут ви побачите інформацію про нас і нашу діяльність.",
+					replyMarkup: MenuModel.GetNewsFeed());
+			}
+
 			//перехід до меню Мій профіль
 			else if (mess == "мій профіль")
 			{
 				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Вітаю, це Ваш профіль",
 					replyMarkup: MenuModel.GetMyProfileMenu());
-				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Вказавши в профілі ім'я, прізввище, номер телефону та email," +
+				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Вказавши в профілі ім'я, прізвище, номер телефону та email," +
 					" ми будемо знати як до Вас звертатися та як Вам надіслати інформацію про смачні пропозиції.");
-
 			}
+
+			//перехід до меню Задонатити на ЗСУ
+
+			else if (mess == "задонатити на зсу!!!")
+			{
+				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Вискакує котик з Банкою для донату.",
+					replyMarkup: MenuModel.GetDonateArmyMenu());
+			}
+
+			//перехід Мій профіль -> Оновити дані
+			else if (mess == "оновити дані")
+			{
+				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Зараз Ви можете оновити свої особисті дані.",
+					replyMarkup: MenuModel.GetUpdateDataMenu());
+			}
+
+
+
+			//перехід Мій профіль -> Мої бонуси
+			else if (mess == "мої бонуси")
+			{
+				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "На вашому балансі * бонусів.",
+					replyMarkup: MenuModel.GetMyBonusMenu());
+			}
+
+			//перехід Мій профіль -> Зробіть нас кращіми!
+			else if (mess == "зробіть нас кращіми!")
+			{
+				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Ми будемо дуже вдячні вам, якщо Ви підкажете що нам" +
+					" змінити щоб бути ще краще.",
+					replyMarkup: MenuModel.GetMakeUsBetterMenu());
+			}
+
 			//повернення з Мій профіль до Головного меню
-			else if (mess == "<-- Повернутися до головного меню")
+			else if (mess == "<-- назад до головного меню")
 			{
 				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Вітаю, Ви у головному меню.",
 					replyMarkup: MenuModel.GetMainMenu());
 			}
-			else if (mess == "меню")
+
+			//повернення з Оновити дані\Мої бонуси\Зробіть нас кращіми до Мій профіль
+			else if (mess == "<-- назад до мого профілю")
 			{
-				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Коли дізнаюсь про " + mess + " я вам підкажу!");
+				await SweetBlues.SendTextMessageAsync(message.Chat.Id, "Вітаю, Ви у меню Мій профіль.",
+					replyMarkup: MenuModel.GetMyProfileMenu());
 			}
 			else
 			{
@@ -69,7 +120,7 @@ namespace SweetBlues
 			if (update.Type == UpdateType.Message)
 			{
 				Message message = update.Message;
-				if(message.Type == MessageType.Text)
+				if (message.Type == MessageType.Text)
 				{
 					await GetMessage(message);
 				}
@@ -83,12 +134,12 @@ namespace SweetBlues
 				}
 			}
 		}
-		
+
 		static async Task BotTakeError(ITelegramBotClient botClient, Exception ex, CancellationToken token)
 		{
 
 		}
 
 
-	}	
+	}
 }
